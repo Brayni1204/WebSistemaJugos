25ce7a8a63f049d451125cffbb35df4ad3f021a4
MyCode <7183255722@untrm.edu.pe>
2025-12-16 06:36:24 -0500

    fix(docker-compose): mejorar el comando de instalación de Composer y agregar comandos de Artisan fix(entrypoint): eliminar comandos de permisos innecesarios en el entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 set -e
-chown -R www-data:www-data /var/www
-chmod -R 777 /var/www/storage /var/www/bootstrap/cache
-export COMPOSER_PROCESS_TIMEOUT=600
-# php artisan migrate --force
 exec "$@"
