f6d9cf7bc26e48f815dac3a6e170901f96cc5536
MyCode <7183255722@untrm.edu.pe>
2026-01-27 20:17:57 -0500

    Replace deprecated React\EventLoop\Factory with React\EventLoop\Loop

--- app/Http/Traits/NotificaWebSocket.php
+++ app/Http/Traits/NotificaWebSocket.php
@@ -4,14 +4,14 @@
 namespace App\Http\Traits;
 
 use Ratchet\Client\Connector;
-use React\EventLoop\Factory;
+use React\EventLoop\Loop;
 
 trait NotificaWebSocket
 {
     public function enviarNotificacion($accion, $mensaje)
     {
         try {
-            $loop = Factory::create();
+            $loop = Loop::get();
             $connector = new Connector($loop);
 
             $payload = json_encode(['action' => $accion, 'message' => $mensaje]);
