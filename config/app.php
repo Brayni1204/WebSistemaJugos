9e442d003753c2fff5a567ac67ce0f62a830f211
MyCode <7183255722@untrm.edu.pe>
2026-02-07 00:25:08 -0500

    chore: set default timezone to America/Lima
    The default timezone has been changed from UTC to America/Lima to better align with the primary user base's location, improving datetime handling for Peruvian users.

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'America/Lima'),
 
 
     'locale' => env('APP_LOCALE', 'es'),
