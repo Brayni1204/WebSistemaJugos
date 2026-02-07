6fd732cc4008d94b85968f817cc3bc96ba6dd149
MyCode <7183255722@untrm.edu.pe>
2026-02-07 01:22:54 -0500

    fix: correct timezone conversion for order timestamps
    The previous method using timezone() on Carbon instances stored as UTC in the database was causing incorrect time display. Use shiftTimezone() to properly convert from UTC to America/Lima timezone.

--- resources/views/admin/pedidos/index.blade.php
+++ resources/views/admin/pedidos/index.blade.php
@@ -54,7 +54,7 @@
                             <tr>
                                 <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente->nombre ?? 'Datos Reservados' }}</td>
-                                <td>{{ $pedido->created_at->timezone('America/Lima')->format('d/m/Y H:i') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($pedido->created_at->toDateTimeString())->shiftTimezone('UTC')->setTimezone('America/Lima')->format('d/m/Y H:i') }}</td>
                                 <td>S/. {{ number_format($pedido->total_pago, 2) }}</td>
                                 <td>
                                     <span
